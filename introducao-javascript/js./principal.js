08a87efece12cb3bc943394768f7d6c1f36c5804
NunesCostaVinicius <163144519+NunesCostaVinicius@users.noreply.github.com>
2024-09-24 11:51:21 -0300

    zom 100

--- introducao-javascript/js./principal.js
+++ introducao-javascript/js./principal.js
@@ -1,1 +1,12 @@
 var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista"
+
+var pacientes = document.querySelector('.paciente');
+for (let contador = 0; contador < pacientes.length; contador++){
+    var dadosPacientes = pacientes[contador];
+    var
+
+
+
+
+}
